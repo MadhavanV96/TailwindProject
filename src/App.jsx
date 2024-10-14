c6d8cdc1b298f861bf341d5c8b4d1bf70ede2c35
MadhavanV96 <13isid4me@gmail.com>
2024-10-15 01:59:11 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TailwindProject from './components/TailwindProject';
+
+const App = () => {
+  return (
+    <div>
+      <TailwindProject />
+    </div>
+  );
+};
+
+export default App;
